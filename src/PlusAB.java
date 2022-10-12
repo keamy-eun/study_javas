cac00981adc2cd542164467a31df1e4922395530
keamy-eun <eun.wonki014@gmail.com>
2022-10-12 14:01:18 +0900

    study_javas

--- src/PlusAB.java
+++ src/PlusAB.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class mminusAB {
+public class PlusAB {
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
         int A = in.nextInt();
