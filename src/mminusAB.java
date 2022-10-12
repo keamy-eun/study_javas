5e2ef4fbe8703acede4e3a96b5526c3dec738407
keamy-eun <eun.wonki014@gmail.com>
2022-10-12 13:58:31 +0900

    study_javas

--- src/mminusAB.java
+++ src/mminusAB.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class main {
+public class mminusAB {
     public static void main(String[] args) throws Exception {
         Scanner in = new Scanner(System.in);
         int A = in.nextInt();
