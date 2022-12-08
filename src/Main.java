1a850c3c916df9d4f2fc146b9b807be307abef92
keamy-eun <eun.wonki014@gmail.com>
2022-12-08 23:35:01 +0900

    coding

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,85 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.math.BigInteger;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
 
 /* 
+1) 문제파악 
 문제
-두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
-
+셀프 넘버는 1949년 인도 수학자 D.R. Kaprekar가 이름 붙였다. 양의 정수 n에 대해서 d(n)을 n과 n의 각 자리수를 더하는 함수라고 정의하자. 예를 들어, d(75) = 75+7+5 = 87이다.
+양의 정수 n이 주어졌을 때, 이 수를 시작해서 n, d(n), d(d(n)), d(d(d(n))), ...과 같은 무한 수열을 만들 수 있다. 
+예를 들어, 33으로 시작한다면 다음 수는 33 + 3 + 3 = 39이고, 그 다음 수는 39 + 3 + 9 = 51, 다음 수는 51 + 5 + 1 = 57이다. 이런식으로 다음과 같은 수열을 만들 수 있다.
+33, 39, 51, 57, 69, 84, 96, 111, 114, 120, 123, 129, 141, ...
+n을 d(n)의 생성자라고 한다. 위의 수열에서 33은 39의 생성자이고, 39는 51의 생성자, 51은 57의 생성자이다. 생성자가 한 개보다 많은 경우도 있다. 예를 들어, 101은 생성자가 2개(91과 100) 있다. 
+생성자가 없는 숫자를 셀프 넘버라고 한다. 100보다 작은 셀프 넘버는 총 13개가 있다. 1, 3, 5, 7, 9, 20, 31, 42, 53, 64, 75, 86, 97
+10000보다 작거나 같은 셀프 넘버를 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
 입력
-첫째 줄에 A와 B가 주어진다. (0 < A,B < 10^10000)
-
+입력은 없다.
 출력
-첫째 줄에 A+B를 출력한다.
+10,000보다 작거나 같은 셀프 넘버를 한 줄에 하나씩 증가하는 순서로 출력한다.
 
 2) 유추파악
-큰 정수 A와 B 의 합 출력
 
 3) 주요 단어 이름 선정
 
-
 4)테스트 케이스
 <출력>
-9223372036854775807 9223372036854775808
---> 
-18446744073709551615
+1
+3
+5
+7
+9
+20
+31
+42
+53
+64
+ |
+ |       <-- a lot more numbers
+ |
+9903
+9914
+9925
+9927
+9938
+9949
+9960
+9971
+9982
+9993
 
 5)프로그래밍 순서
-int는 자리수가 10 이상 입력되지 않는다. 
-Biginteger를 사용한다.
+
 */
 
 public class Main {
-        public static void main(String[] args) {     
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static ArrayList<Integer> arrayList;
+    // 셀프넘버 생성 함수
+    public void fun(){
+        for(int i=1; i<10001; i++){
+            int num1 = i/1000%10;
+            int num2 = i/100%10;
+            int num3 = i/10%10;
+            int num4 = i%10;
+            //각 자리수 합
+            int sum = i+num1+num2+num3+num4;
+            if(arrayList.contains(sum)) {
+                arrayList.remove(Integer.valueOf(sum));
+            }
+        }
+    }
+    public static void main(String[] args) {     
 
         try {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            String A = st.nextToken();
-            
+            arrayList = new ArrayList<>();
+            for(int i=1; i<10001; i++) arrayList.add(i);  //1~9999 배열 생성
 
+            Main main = new Main();
+            main.fun();
 
-            int a = Integer.parseInt(A);
-            int b = Integer.parseInt(B);
-            bw.write((a+b)+"\n");
-            bw.flush();
-            br.close();
-            bw.close();
+            for(int i=0; i<arrayList.size(); i++){
+                System.out.println(arrayList.get(i));
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
+        
     }
 }
